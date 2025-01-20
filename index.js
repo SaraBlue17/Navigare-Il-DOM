c20aa3351ea6cfad6506e99f6950116117db1a01
Sara Rampazzo <sararampazzo.ph@gmail.com>
2025-01-20 22:43:52 +0100

    aggiunto appunti della video soluzione

--- index.js
+++ index.js
@@ -21,3 +21,17 @@ console.log(
   "L'elemento fratello precedente del secondo li:",
   previusSibling.innerText
 );
+
+// Video soluzione
+
+const html = document.documentElement;
+// console.log(html);
+const body = html.children(1);
+// console.log(body);
+const ul = body.children[0];
+const secondLi = ul.children[1];
+
+console.log(ul.parentElement);
+console.log(secondLi.innerHTML);
+console.log(secondLi.nextElementSibling.innerHTML);
+console.log(secondLi - previousElementSibling.innerHTML);
